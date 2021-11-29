65b1d54ec3441c11615da0bd23e1faf0b8e424a2
Gabor Toth <>
2021-11-29 08:49:11 +0100

    implementation

---
+++ src/main/java/model/BucketFill.java
@@ -0,0 +1,44 @@
+package model;
+
+import utils.Util;
+
+import java.util.Objects;
+
+public class BucketFill {
+
+    private int  x;
+    private int  y;
+    private char character;
+
+    public BucketFill(int x, int y, char character) {
+        Util.checkValuesArePositive(x, y);
+        this.x = x;
+        this.y = y;
+        this.character = character;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BucketFill that = (BucketFill) o;
+        return x == that.x && y == that.y && character == that.character;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, character);
+    }
+}
